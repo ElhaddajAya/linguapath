ed375330c5fcca142ca9d264df1f60b6fb82e854
EL HADDAJ Aya <152729348+ElhaddajAya@users.noreply.github.com>
2026-03-29 18:54:03 +0100

    Fix password hashing middleware to call next()

--- backend/models/User.js
+++ backend/models/User.js
@@ -44,10 +44,11 @@ const UserSchema = new mongoose.Schema(
 )
 
 // Hacher le mot de passe avant chaque sauvegarde
-UserSchema.pre('save', async function () {
-  if (!this.isModified('password')) return
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
+  next()
 })
 
 // Méthode pour comparer les mots de passe
