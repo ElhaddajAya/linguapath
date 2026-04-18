cb536f1445472780426db34e38f94774f702b79e
MalakElFadil <fadilmalak922@gmail.com>
2026-04-18 15:02:00 +0100

    page Profile et App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login  from './pages/Login'
-import Signup from './pages/Signup'
+import Login   from './pages/Login'
+import Signup  from './pages/Signup'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login"  element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login"   element={<Login />} />
+        <Route path="/signup"  element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/"        element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
